60720f82ba127c64c5fbf78d874c68d7a6ed5df5
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-03-09 00:45:28 -0500

    testing task 3

--- answers/global_average_recommender.py
+++ answers/global_average_recommender.py
@@ -22,9 +22,9 @@ ratings = spark.createDataFrame(ratingsRDD)
 (training, test) = ratings.randomSplit([0.8, 0.2], seed=desired_seed)
 
 global_avg = training.groupby().avg().collect()[0]['avg(rating)']
-print(global_avg)
-training = training.withColumn('global_avg', lit(global_avg))
-test = test.withColumn('global_avg', lit(global_avg))
+
+training = training.withColumn('prediction', lit(global_avg))
+test = test.withColumn('prediction', lit(global_avg))
 
 evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
                             predictionCol="prediction")
